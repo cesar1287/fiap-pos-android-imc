be4fbde7c6cdf07fdb4dae3c6e53f98d1e52daed
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-02-14 20:11:41 -0300

    adjusting layout

--- app/src/main/java/com/github/cesar1287/imc/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/imc/MainActivity.kt
@@ -2,12 +2,16 @@ package com.github.cesar1287.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.cesar1287.imc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
